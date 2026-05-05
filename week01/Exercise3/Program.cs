214b4cc81cf8ab93c8b63a8a9a052cddeed84c04
Rigfen <Rigfen@gmail.com>
2026-05-05 21:23:58 +0100

    asdf

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+
+       Random randomGenerator = new Random();
+int number = randomGenerator.Next(1, 100);
+
+int guess = -1;
+
+while (guess != number)
+{
+    Console.Write("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+
+    if (guess < number)
+            {
+                Console.WriteLine("Too Low Try again!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Too High Try Again");
+
+            }
+            else
+            {
+                Console.WriteLine("You Got it!");
+            }
+
 }
+
+Console.WriteLine($"The random number is: {number}");
+}}
