c21ae625a54c28ed9600c271149049493d34bb29
Rigfen <Rigfen@gmail.com>
2026-05-05 19:16:55 +0100

    asdf

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("I am learning how to use Visual Studio Code and GitHub.");
+
+        int number = 5;
+        number = number + 10;
+        number = 9 * number;
+
+
+        Console.Write("what is your first name? " );
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {lastname}, {name}! Welcome to C# programming!");
+    
     }
 }
