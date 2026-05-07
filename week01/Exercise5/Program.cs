1040e196e788973e602607d0550441fd0c480bb2
Rigfen <Rigfen@gmail.com>
2026-05-07 21:34:29 +0100

    asdf

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
+    static void DisplayMessage()
+
+    {
+        Console.WriteLine("Hello World!");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        PromptUser();
+        int result = AddNumbers(5, 7);
+        Console.Write($"The sum of the numbers is: {result}");
+    }
+
+    static void PromptUser()
+    {
+        Console.Write("What is your name? ");
+        string name= Console.ReadLine();
+        Console.WriteLine($"Hello {name}!");
+    }
+
+    static int AddNumbers (int num1, int num2)
+    {
+        int sum = num1 + num2;
+        return sum;
     }
 }
