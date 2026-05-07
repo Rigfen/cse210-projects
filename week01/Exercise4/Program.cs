699cd70b8a128489c88632ed48bb34bc923400e4
Rigfen <Rigfen@gmail.com>
2026-05-07 19:58:46 +0100

    asdf

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,20 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers= new List<int>();
+        List<int> numbers = new List<int>();
+        
+        
         int userNumber = -1;
         while (userNumber != 0)
         {
             Console.Write("Enter a number (0 to quit): ");
-            userNumber = int.Parse(Console.ReadLine());
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
             if (userNumber != 0)
             {
                 numbers.Add(userNumber);
             }
         }
+
         int sum = 0;
         foreach (int number in numbers)
         {
@@ -27,18 +34,18 @@ class Program
         float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
 
-        
+    
         int max = numbers[0];
 
         foreach (int number in numbers)
         {
             if (number > max)
             {
-                // if this number is greater than the max, we have found the new max!
+        
                 max = number;
             }
         }
 
         Console.WriteLine($"The max is: {max}");
     }
-        }
+}
