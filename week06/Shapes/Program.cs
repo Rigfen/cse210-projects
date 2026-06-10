ab5047863b41bbc81056666b098484dec496b9f7
Rigfen <Rigfen@gmail.com>
2026-06-10 21:40:24 +0100

    working on it

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Test individual shapes
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Color: {square.GetColor()}");
+        Console.WriteLine($"Area: {square.GetArea()}");
+
+        Console.WriteLine();
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Blue", 4));
+        shapes.Add(new Rectangle("Green", 5, 3));
+        shapes.Add(new Circle("Yellow", 2));
+
+        // Display all shapes
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
     }
 }
